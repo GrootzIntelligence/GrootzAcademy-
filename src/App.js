5178914f64d3b1858da2d7f83fd9fc9e90285832
Shiv Rai <sshaktirai@gmail.com>
2024-10-01 12:35:50 +0530

    Added navbar and headstart section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
-
 import './App.css';
-
+import Navbar from './components/Navbar';
+import HeadStart from './components/HeadStart';
 function App() {
   return (
-    <div className="App">
+    <div>
+      <Navbar />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <HeadStart />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
     </div>
   );
 }
